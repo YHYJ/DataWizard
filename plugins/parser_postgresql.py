27a79851f3628c341ad7600ad56979379759305d
YJ-Arch <yj1516268@outlook.com>
2021-04-08 18:50:44 +0800

    建立插件

---
+++ plugins/parser_postgresql.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File: parser_postgresql.py
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2021-04-08 18:48:04
+
+Description: 为PostgreSQL进行原始数据解析
+"""
+
+
+def parse_system_monitor(datas):
+    """解析系统监视信息
+
+    :datas: TODO
+    :returns: TODO
+
+    """
+    pass
